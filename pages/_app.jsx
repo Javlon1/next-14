618e9951346efa7090ce77159abe4678c362c69d
Javlon1 <muhamedmijotot@gmail.com>
2023-12-21 14:50:44 +0300

    404

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,39 @@
 import Layout from "@/app/components/layout/Layout";
 import '@/app/assets/globals.scss';
 import { Provider } from "@/app/components/ui/Context/Context";
+import { useEffect } from "react";
 
 const myApp = ({ Component, pageProps }) => {
-    
+
+    // 
+    useEffect(() => {
+        const handleContextMenu = (e) => {
+            e.preventDefault();
+        };
+
+        const handleKeyDown = (e) => {
+            if (e.keyCode === 123) {
+                e.preventDefault();
+            } else if (e.ctrlKey && e.shiftKey && e.keyCode === 'I'.charCodeAt(0)) {
+                e.preventDefault();
+            } else if (e.ctrlKey && e.shiftKey && e.keyCode === 'C'.charCodeAt(0)) {
+                e.preventDefault();
+            } else if (e.ctrlKey && e.shiftKey && e.keyCode === 'J'.charCodeAt(0)) {
+                e.preventDefault();
+            } else if (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)) {
+                e.preventDefault();
+            }
+        };
+
+        document.addEventListener('contextmenu', handleContextMenu);
+        document.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            document.removeEventListener('contextmenu', handleContextMenu);
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+    //
     return (
         <Provider>
             <Layout>
